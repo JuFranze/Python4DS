86c00768a4975e1b5f90fd44ba489e2d0107549d
JuFranze <55830611+JuFranze@users.noreply.github.com>
2022-03-10 18:02:46 +0000

    homework week 1

---
+++ week-1-content/homework_w1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Variables
+area = ['East', 'West', 'North', 'East']
+candidates= [100, 200, 300, 400]
+matrix_1 = [[1, 2], [2, 4]]
+
+
+# find out what type your variables are
+
+
+
+
+#Find the max and min of the candidates list 
+
+
+#Create the sum of candidates with initial value of 50 to the sum
+
+
+
+#Create the STD of the candidates list & matrix_1
+# Standard Deviation (STD) is the measure by which the elements of a set are deviated or dispersed from the mean.
+
+
+
+
+# create graph with pandas, showing a barchart with candidates per area (use a Dataframe)
